6be43d192ea073ed3f81194f788efad3c6da4fd1
Eslam Basem <eslambasem83@gmail.com>
2025-10-16 15:27:40 +0300

    firts commit

--- script.py
+++ script.py
@@ -8,7 +8,7 @@ from imblearn.over_sampling import SMOTE
 from PIL import Image
 
 ## skelarn -- preprocessing
-from sklearn.model_selection import train_test_splits
+from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.pipeline import Pipeline, FeatureUnion
@@ -41,7 +41,7 @@ X = df.drop(columns=['Exited'], axis=1)
 y = df['Exited']
 
 ## Split to train and test
-X_train, X_test, y_train, y_test = train_test_splits(X, y, test_size=0.2, shuffle=True, random_state=45, stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True, random_state=45, stratify=y)
 
 
 ## --------------------- Data Processing ---------------------------- ##
